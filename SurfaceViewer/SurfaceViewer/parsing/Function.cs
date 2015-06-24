ccc819c77fd0c961bd946acb9f34fd4328d84483
Vladislav Shirinkin <sapiens1995@gmail.com>
2015-06-24 21:53:26 +0500

    parser

---
+++ SurfaceViewer/SurfaceViewer/parsing/Function.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationUsingOpenGLAndWPF.Parsing
+{
+
+    public interface Function                                              //интерфейс для всех функций
+    {
+        Func<double[], double[]> getFunction();              //метод должен возвращать делегат на функцию, параметром которой является массив чисел, функция тоже возвращает массив чисел
+    }
+
+    public abstract class CommonFunction : Function                   //класс для обычной функции (не вектор-функции) нескольких переменных
+    {
+
+        public abstract Func<double[], double> getCommonFunction(); //метод, который должен возвращать делегат на обычную функцию
+
+        public Func<double[], double[]> getFunction()               //заглушка метода, обращение к методу выше
+        {
+            return p => new double[] { getCommonFunction()(p) };
+        }
+    }
+
+    public class Variable : CommonFunction                          //переменная тоже являестя функцией нескольких переменных, возвращает она значение одой из переменных
+    {
+
+        public int index;
+        
+        public override Func<double[], double> getCommonFunction()
+        {
+            return p => p[index];
+        }
+    }
+
+    public class OneVarFunction : CommonFunction                    //класс для обычной функции одного переменного
+    {
+
+        public Function arg;                                        //есть смысл создать поле аргумента, так как этот класс является конечным в данной ветке
+
+        public Func<double, double> function = p => Double.NaN;     //делегат на функцию одного аргумента, стандартная инициализация - возврат неопределенного числа
+
+        public override Func<double[], double> getCommonFunction()  //заглушка метода, обращение к делегату с аргументом в виде запрошенного значения с поля arg
+        {
+            if (arg != null)
+                return p => function(arg.getFunction()(p)[0]);
+            else
+                return p => function(0);
+        }
+    }
+
+    public class Sum : CommonFunction                               //функция суммы нескольких элементов с разными знаками, элементы могут быть любыми функциями
+    {
+
+        public List<Function> operands = new List<Function>();      //контейнер, который содержит элементы
+        public List<bool> signs = new List<bool>();                 //знак (-,+) для каждого элемента
+
+        private double func(double[] p)                             //метод для подсчёта суммы
+        {
+            double result = 0;
+            for (int i = 0; i < operands.Count; i++)
+            {
+                if (signs[i])
+                {
+                    result += operands[i].getFunction()(p)[0];
+                }
+                else
+                {
+                    result -= operands[i].getFunction()(p)[0];
+                }
+            }
+            return result;
+        }
+
+        public override Func<double[], double> getCommonFunction()  //заглушка метода, обращается к методу выше
+        {
+            return func;
+        }
+    }
+    public class Mul : CommonFunction                               //произведение элементов с двумя разными степенями.
+    {
+
+        public List<Function> operands = new List<Function>();      //аналог
+        public List<bool> powers = new List<bool>();                //массив степеней, означает либо -1 степень при произведении, либо 1 степень
+
+        private double func(double[] p)                             //аналог
+        {
+            double result = operands[0].getFunction()(p)[0];
+            for (int i = 1; i < operands.Count; i++)
+            {
+                if (powers[i])
+                {
+                    result *= operands[i].getFunction()(p)[0];
+                }
+                else
+                {
+                    result /= operands[i].getFunction()(p)[0];
+                }
+            }
+            return result;
+        }
+
+        public override Func<double[], double> getCommonFunction()  //аналог
+        {
+            return func;
+        }
+    }
+
+    public class Pow : CommonFunction                               //степень
+    {
+        public List<Function> baseAndPower=new List<Function>();                         //основание и показатели последовательных степеней
+
+        private double compute(double[] p)                          //осуществляет математическую операцию подсчёта степени несколько раз
+        {
+            if (baseAndPower.Count == 1) return baseAndPower[0].getFunction()(p)[0];
+            else
+            {
+                double result = baseAndPower[baseAndPower.Count-1].getFunction()(p)[0];
+                for (int i = baseAndPower.Count-2; i >=0; i--)
+                {
+                    result= Math.Pow(baseAndPower[i].getFunction()(p)[0],result);
+                }
+                return result;
+            }
+        }
+
+        public override Func<double[], double> getCommonFunction()  //заглушка метода, обращается к методу выше
+        {
+            return p => compute(p);
+        }
+    }
+
+}
---
+++ SurfaceViewer/SurfaceViewer/parsing/MathParserObjective.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ApplicationUsingOpenGLAndWPF.Parsing
+{
+
+
+    class MathParserObjective
+    {
+        private string expression;
+        private int position;
+        private char look;
+        public readonly Dictionary<string, Func<double, double>> builtInFunctions
+            = new Dictionary<string, Func<double, double>>();
+        private Dictionary<string, int> variables
+            = new Dictionary<string, int>();
+
+        public void InitBuiltinFunctions()
+        {
+            builtInFunctions["abs"] = Math.Abs;
+            builtInFunctions["acos"] = Math.Acos;
+            builtInFunctions["acosh"] = MathNet.Numerics.Trig.Acosh;
+            builtInFunctions["asin"] = Math.Asin;
+            builtInFunctions["asinh"] = MathNet.Numerics.Trig.Asinh;
+            builtInFunctions["atan"] = Math.Atan;
+            builtInFunctions["atanh"] = MathNet.Numerics.Trig.Atanh;
+            builtInFunctions["cbrt"] = x => Math.Pow(x, 1 / 3);
+            builtInFunctions["ceil"] = Math.Ceiling;
+            builtInFunctions["cos"] = Math.Cos;
+            builtInFunctions["cosh"] = Math.Cosh;
+            builtInFunctions["exp"] = Math.Exp;
+            builtInFunctions["floor"] = Math.Floor;
+            builtInFunctions["log"] = Math.Log;
+            builtInFunctions["log10"] = Math.Log10;
+            builtInFunctions["signum"] = x => Math.Sign(x);
+            builtInFunctions["sin"] = Math.Sin;
+            builtInFunctions["sinh"] = Math.Sinh;
+            builtInFunctions["sqrt"] = Math.Sqrt;
+            builtInFunctions["tan"] = Math.Tan;
+            builtInFunctions["tanh"] = Math.Tanh;
+            builtInFunctions["rtd"] = x => x / Math.PI * 180;
+            builtInFunctions["dtr"] = x => x * Math.PI / 180;
+        }
+
+        public static int CharToInt(char c)
+        {
+            return Convert.ToInt32(c) - Convert.ToInt32('0');
+        }
+
+        private bool IsDigit(char c)
+        {
+            return char.IsDigit(c);
+        }
+
+        private bool IsAlpha(char c)
+        {
+            return char.IsLetter(c);
+        }
+
+        private static bool IsAddop(char c)
+        {
+            return "+-".Contains(c);
+        }
+
+        private bool IsMulop(char c)
+        {
+            return "*/".Contains(c);
+        }
+        private bool IsCaret(char c)
+        {
+            return c == '^';
+        }
+
+        private bool IsWhiteSpace(char c)
+        {
+            return Char.IsWhiteSpace(c);
+        }
+
+        private void Expected(string what)
+        {
+            throw new ParserException(what + " expected");
+        }
+
+        private void SkipWhiteSpace()
+        {
+            while (IsWhiteSpace(look))
+            {
+                GetChar();
+            }
+        }
+        private double GetNum()
+        {
+            string result = "";
+            if (!IsDigit(look)) Expected("Number in position " + position);
+            while (IsDigit(look))
+            {
+                result += look;
+                GetChar();
+            }
+            if (look == '.')
+            {
+                result += look;
+                GetChar();
+
+                while (IsDigit(look))
+                {
+                    result += look;
+                    GetChar();
+                }
+            }
+            SkipWhiteSpace();
+            return double.Parse(result, CultureInfo.InvariantCulture.NumberFormat);
+        }
+
+        private string GetName()
+        {
+            string result = "";
+            if (!IsAlpha(look)) Expected("Name");
+            while (IsDigit(look) || IsAlpha(look))
+            {
+                result += look;
+                GetChar();
+            }
+            SkipWhiteSpace();
+            return result;
+        }
+
+        private char Read()
+        {
+            if (position < expression.Length)
+                return expression[position++];
+            return '\0';
+        }
+
+        private void GetChar()
+        {
+            look = Read();
+        }
+
+        private void Match(char c)
+        {
+            if (look == c)
+            {
+                GetChar();
+                SkipWhiteSpace();
+            }
+            else
+                Expected(string.Format("'{0}'", c));
+        }
+
+        public MathParserObjective(string expression, string[] variables)
+        {
+            InitBuiltinFunctions();
+            this.expression = expression;
+            for (int i = 0; i < variables.Length; i++)
+            {
+                this.variables[variables[i]] = i;
+            }
+            Reset();
+        }
+
+        public void Reset()
+        {
+            position = 0;
+            GetChar();
+            SkipWhiteSpace();
+        }
+
+        public Func<double[], double> Parse()
+        {
+            Reset();
+            return ((CommonFunction)Expression()).getCommonFunction();
+        }
+
+        private Function Expression()
+        {
+            Sum sum = new Sum();
+            if (!IsAddop(look))
+            {
+                sum.operands.Add(Term());
+                sum.signs.Add(true);
+            }
+            while (IsAddop(look))
+            {
+                switch (look)
+                {
+                    case '+':
+                        Match('+');
+                        sum.operands.Add(Term());
+                        sum.signs.Add(true);
+                        break;
+                    case '-':
+                        Match('-');
+                        sum.operands.Add(Term());
+                        sum.signs.Add(false);
+                        break;
+                    default:
+                        throw new Exception();
+                }
+            }
+            return sum;
+        }
+
+        private Function Term()
+        {
+            Mul mul = new Mul();
+            mul.operands.Add(Factor());
+            mul.powers.Add(true);
+            while (IsMulop(look))
+            {
+                switch (look)
+                {
+                    case '*':
+                        Match('*');
+                        mul.operands.Add(Factor());
+                        mul.powers.Add(true);
+                        break;
+                    case '/':
+                        Match('/');
+                        mul.operands.Add(Factor());
+                        mul.powers.Add(false);
+                        break;
+                    default:
+                        throw new Exception();
+                }
+            }
+            return mul;
+        }
+
+        private Function Factor()
+        {
+            Pow pow = new Pow();
+            pow.baseAndPower.Add(Power());
+            while (IsCaret(look))
+            {
+                Match('^');
+                pow.baseAndPower.Add(Power());
+            }
+            return pow;
+        }
+
+        private Function Power()
+        {
+            Function result = null;
+            if (look == '(')
+            {
+                Match('(');
+                result = Expression();
+                Match(')');
+                return result;              //значимое изменение
+            }
+            if (IsAlpha(look))
+            {
+                var name = GetName();
+                if (look == '(')
+                {
+                    Match('(');
+                    result = new OneVarFunction();
+                    ((OneVarFunction)result).function = builtInFunctions[name];
+                    ((OneVarFunction)result).arg = Expression();
+                    Match(')');
+                }
+                else
+                {
+                    result = new Variable();
+                    ((Variable)result).index = variables[name];
+                }
+            }
+            else
+            {
+                result = new OneVarFunction();
+                double constant=GetNum();
+                ((OneVarFunction)result).function = x => constant;
+            }
+
+            return result;
+        }
+
+        public static Func<double[], double> ParseExpression(string expression, string[] variables)
+        {
+            return new MathParserObjective(expression, variables).Parse();
+        }
+    }
+}

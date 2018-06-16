d9643de657ba1de0d0fb3e7d14cd2bf2a3e7e943
Владислав Ширинкин <vladislav.shirinkin@outlook.com>
2018-06-16 23:06:37 +0500

    анадитические нормали и примеры фигур

--- SurfaceViewer/SurfaceViewer/Functions/Surface.cs
+++ SurfaceViewer/SurfaceViewer/Functions/Surface.cs
@@ -5,64 +5,76 @@ using System.Text;
 using System.Threading.Tasks;
 using MathNet.Numerics.Differentiation;
 using Symbolic.Functions;
+using Symbolic;
 
 namespace SurfaceViewer.Functions
 {
     class Surface
     {
 
-        private VectorFunction r, normal;
+        private Vector3 r, normal;
 
-        public Surface(VectorFunction r)
+        public Surface(string xExpr, string yExpr, string zExpr, string u, string v, bool analytic)
         {
-            this.r = r;
+            var domain = ":" + u + ";" + v;
+            var x = (CommonFunction)(xExpr + domain);
+            var y = (CommonFunction)(yExpr + domain);
+            var z = (CommonFunction)(zExpr + domain);
+            r = new Vector3(x, y, z);
+            if (analytic)
+                normal = getNormalAnalitic(xExpr, yExpr, zExpr, u, v);
+            else
+                normal = getNormalNumeric(r);
+        }
 
-            List<CommonFunction> components = r.components;
+        private Vector3 getNormalAnalitic(string xExpr, string yExpr, string zExpr, string u, string v)
+        {
+            var d = ":" + u + ";" + v;
+
+            var xonu = Derivative.GetDerivative(xExpr, u, false);
+            var xonv = Derivative.GetDerivative(xExpr, v, false);
+            var yonu = Derivative.GetDerivative(yExpr, u, false);
+            var yonv = Derivative.GetDerivative(yExpr, v, false);
+            var zonu = Derivative.GetDerivative(zExpr, u, false);
+            var zonv = Derivative.GetDerivative(zExpr, v, false);
+
+            var v1 = new Vector3(xonu + d, yonu + d, zonu + d);
+            var v2 = new Vector3(xonv + d, yonv + d, zonv + d);
+
+            var normalNotNormalized = (v1 ^ v2);
+            var len = (CommonFunction)("sqrt(" + (normalNotNormalized * normalNotNormalized).Print() + ")" + d);
+            return normalNotNormalized / len;
+        }
+
+        private Vector3 getNormalNumeric(Vector3 r)
+        {
             List<DefinedCommonFunction> derivativesU = new List<DefinedCommonFunction>();
             List<DefinedCommonFunction> derivativesV = new List<DefinedCommonFunction>();
             NumericalDerivative derivative = new NumericalDerivative();
-            derivativesU.Add(new DefinedCommonFunction(derivative.CreatePartialDerivativeFunctionHandle(components[0].getCommonFunction(), 0, 1), "null"));
-            derivativesV.Add(new DefinedCommonFunction(derivative.CreatePartialDerivativeFunctionHandle(components[0].getCommonFunction(), 1, 1), "null"));
-            derivativesU.Add(new DefinedCommonFunction(derivative.CreatePartialDerivativeFunctionHandle(components[1].getCommonFunction(), 0, 1), "null"));
-            derivativesV.Add(new DefinedCommonFunction(derivative.CreatePartialDerivativeFunctionHandle(components[1].getCommonFunction(), 1, 1), "null"));
-            derivativesU.Add(new DefinedCommonFunction(derivative.CreatePartialDerivativeFunctionHandle(components[2].getCommonFunction(), 0, 1), "null"));
-            derivativesV.Add(new DefinedCommonFunction(derivative.CreatePartialDerivativeFunctionHandle(components[2].getCommonFunction(), 1, 1), "null"));
+            derivativesU.Add(new DefinedCommonFunction(derivative.CreatePartialDerivativeFunctionHandle(r.X.Invoke, 0, 1), "null"));
+            derivativesV.Add(new DefinedCommonFunction(derivative.CreatePartialDerivativeFunctionHandle(r.X.Invoke, 1, 1), "null"));
+            derivativesU.Add(new DefinedCommonFunction(derivative.CreatePartialDerivativeFunctionHandle(r.Y.Invoke, 0, 1), "null"));
+            derivativesV.Add(new DefinedCommonFunction(derivative.CreatePartialDerivativeFunctionHandle(r.Y.Invoke, 1, 1), "null"));
+            derivativesU.Add(new DefinedCommonFunction(derivative.CreatePartialDerivativeFunctionHandle(r.Z.Invoke, 0, 1), "null"));
+            derivativesV.Add(new DefinedCommonFunction(derivative.CreatePartialDerivativeFunctionHandle(r.Z.Invoke, 1, 1), "null"));
 
-            VectorFunction v1 = new VectorFunction(derivativesU);
-            VectorFunction v2 = new VectorFunction(derivativesV);
+            Vector3 v1 = new Vector3(derivativesU);
+            Vector3 v2 = new Vector3(derivativesV);
 
-            VectorFunction normalLength = new Vector3Mul(v1, v2);
-            CommonFunction length = new Length(normalLength);
-
-            List<bool> normalPow = new List<bool>();
-            normalPow.Add(true); normalPow.Add(false);
-
-            List<CommonFunction> normalx = new List<CommonFunction>();
-            normalx.Add(normalLength.components[0]); normalx.Add(length);
-
-            List<CommonFunction> normaly = new List<CommonFunction>();
-            normaly.Add(normalLength.components[1]); normaly.Add(length);
-
-            List<CommonFunction> normalz = new List<CommonFunction>();
-            normalz.Add(normalLength.components[2]); normalz.Add(length);
-
-            List<CommonFunction> normalCoordinates = new List<CommonFunction>();
-            normalCoordinates.Add(new Mul(normalx, normalPow));
-            normalCoordinates.Add(new Mul(normaly, normalPow));
-            normalCoordinates.Add(new Mul(normalz, normalPow));
-
-            normal = new VectorFunction(normalCoordinates);
+            var normalNotNormalized = (v1 ^ v2);
+            var len = new OneVarFunction(Math.Sqrt, normalNotNormalized * normalNotNormalized, null);
+            return normalNotNormalized / len;
         }
 
         public float[] getVertex(double u, double v)
         {
-            double[] result = r.getFunction()(new double[] { u, v });
+            double[] result = r.InvokeVec(new double[] { u, v });
             return new float[] { (float)result[0], (float)result[1], (float)result[2] };
         }
 
         public float[] getNormal(double u, double v)
         {
-            double[] result = normal.getFunction()(new double[] { u, v });
+            double[] result = normal.InvokeVec(new double[] { u, v });
             return new float[] { (float)result[0], (float)result[1], (float)result[2] };
         }
     }
--- SurfaceViewer/SurfaceViewer/MainWindow.xaml.cs
+++ SurfaceViewer/SurfaceViewer/MainWindow.xaml.cs
@@ -52,7 +52,7 @@ namespace SurfaceViewer
             gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
             gl.LoadIdentity();
             gl.Translate(0.0f, 0.0f, -3.0f);
-            rotate = (float)((DateTime.Now - begin).TotalSeconds)*10;
+            rotate = (float)((DateTime.Now - begin).TotalSeconds) * 10;
             gl.Rotate(rotate, 0.0f, 1.0f, 0.0f);
             if (mesh != null)
                 mesh.draw(gl);
@@ -77,7 +77,7 @@ namespace SurfaceViewer
 
             gl.Material(OpenGL.GL_FRONT, OpenGL.GL_DIFFUSE, new float[] { .8f, 0, 0, 1 });
             gl.Material(OpenGL.GL_FRONT, OpenGL.GL_SPECULAR, new float[] { .3f, 0, 0, 1 });
-            gl.Material(OpenGL.GL_BACK, OpenGL.GL_DIFFUSE, new float[] { 0, 0, 1, 1 }); 
+            gl.Material(OpenGL.GL_BACK, OpenGL.GL_DIFFUSE, new float[] { 0, 0, 1, 1 });
             gl.Material(OpenGL.GL_BACK, OpenGL.GL_SPECULAR, new float[] { .2f, 0, 0, 1 });
             enterData();
             begin = DateTime.Now;
@@ -106,19 +106,91 @@ namespace SurfaceViewer
 
         public void enterData()
         {
-            List<CommonFunction> coordinates = new List<CommonFunction>();
-            coordinates.Add(Parser.ParseExpressionObject(x.Text, new string[] { "u", "v" }));
-            coordinates.Add(Parser.ParseExpressionObject(y.Text, new string[] { "u", "v" }));
-            coordinates.Add(Parser.ParseExpressionObject(z.Text, new string[] { "u", "v" }));
-            VectorFunction r = new VectorFunction(coordinates);
-            surface = new Surface(r);
-            double left = Parser.ParseExpression(this.left.Text, null)(null);
-            double right = Parser.ParseExpression(this.right.Text, null)(null);
-            double bottom = Parser.ParseExpression(this.bottom.Text, null)(null);
-            double top = Parser.ParseExpression(this.top.Text, null)(null);
-            double seed = Parser.ParseExpression(this.seed.Text, null)(null);
+            surface = new Surface(x.Text, y.Text, z.Text, "s", "t", (bool)(analitics.IsChecked));
+            double left = ((CommonFunction)(this.left.Text + ":null")).Invoke(null);
+            double right = ((CommonFunction)(this.right.Text + ":null")).Invoke(null);
+            double bottom = ((CommonFunction)(this.bottom.Text + ":null")).Invoke(null);
+            double top = ((CommonFunction)(this.top.Text + ":null")).Invoke(null);
+            double seed = ((CommonFunction)(this.seed.Text + ":null")).Invoke(null);
             mesh = new Mesh(surface, seed, left, right, bottom, top, true, (bool)this.net.IsChecked);
             mesh.computeMesh();
+        }
+
+        private void sphere_Click(object sender, RoutedEventArgs e)
+        {
+            x.Text = "cos(s)*cos(t)";
+            y.Text = "cos(s)*sin(t)";
+            z.Text = "sin(s)";
+            left.Text = "dtr(-90)";
+            right.Text = "dtr(90)";
+            bottom.Text = "dtr(-180)";
+            top.Text = "dtr(180)";
+        }
+
+        private void helicoid_Click(object sender, RoutedEventArgs e)
+        {
+            x.Text = "s*cos(t)";
+            y.Text = "t*0.5";
+            z.Text = "s*sin(t)";
+            left.Text = "-0.7";
+            right.Text = "0.7";
+            bottom.Text = "-3";
+            top.Text = "3";
+        }
+
+        private void parabol_Click(object sender, RoutedEventArgs e)
+        {
+            x.Text = "s";
+            y.Text = "s^2+t^2-1";
+            z.Text = "t";
+            left.Text = "-0.5";
+            right.Text = "0.5";
+            bottom.Text = "-1";
+            top.Text = "1";
+        }
+
+        private void monkey_Click(object sender, RoutedEventArgs e)
+        {
+            x.Text = "s";
+            z.Text = "t";
+            y.Text = "s^3-3*s*t^2";
+            left.Text = "-0.7";
+            right.Text = "0.7";
+            bottom.Text = "-0.7";
+            top.Text = "0.7";
+        }
+
+        private void hyperbol_Click(object sender, RoutedEventArgs e)
+        {
+            x.Text = "s";
+            y.Text = "0.7*s^2-0.7*t^2";
+            z.Text = "t";
+            left.Text = "-0.5";
+            right.Text = "0.5";
+            bottom.Text = "-1";
+            top.Text = "1";
+        }
+
+        private void thorus_Click(object sender, RoutedEventArgs e)
+        {
+            x.Text = "(0.7+0.2*cos(s))*cos(t)";
+            y.Text = "(0.7+0.2*cos(s))*sin(t)";
+            z.Text = "0.2*sin(s)";
+            left.Text = "dtr(-180)";
+            right.Text = "dtr(180)";
+            bottom.Text = "dtr(-180)";
+            top.Text = "dtr(180)";
+        }
+
+        private void thorus_broken_Click(object sender, RoutedEventArgs e)
+        {
+            x.Text = "(0.5+0.5*cos(s))*cos(t)";
+            y.Text = "(0.5+0.5*cos(s))*sin(t)";
+            z.Text = "0.5*sin(s)";
+            left.Text = "dtr(-150)";
+            right.Text = "dtr(150)";
+            bottom.Text = "dtr(-90)";
+            top.Text = "dtr(90)";
         }
     }
 }
